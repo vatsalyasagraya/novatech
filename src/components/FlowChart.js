3dc15886d46a4eb9d5b3d33a11645e15081ac40e
Moxesh555 <2020uec1080@mnit.ac.in>
2023-04-05 22:05:49 +0530

    new commit

--- src/components/FlowChart.js
+++ src/components/FlowChart.js
@@ -9,24 +9,104 @@ function FlowChart() {
     const refB=useRef("null");
     const refIr=useRef("null");
     const refRAM=useRef("null");
-    
+    const refROM=useRef("null");
+    const refMAR=useRef(0);
+    const refPC=useRef(0);
+
     const [a, setA] = useState("");
     const [b, setB] = useState("");
 
-    function handleClick(){
+    function delay(ms) {
+        return new Promise(resolve => setTimeout(resolve, ms));
+    }
+      
+    async function handleClick(){
         let commands = a.split("\n");
         for(let i=0;i<commands.length;i++)
         {
         const decimalA = parseInt(refA.current.value, 2);
         const decimalB = parseInt(refB.current.value, 2);
+        //const dPC= parseInt(refPC.current.value, 2);
         let command=a.split("\n")[i].split(" ")[0];
-        refIr.current.value=command;
+        
         switch (command) 
         {
             case "MVI":
+                /////////////////////////////////////////////////
+                refROM.current.value="T0: PC(E), MAR(E,L)\n";
+                refPC.current.style.backgroundColor = 'rgba(0, 128, 0, 0.568)';
+                refPC.current.style.color = 'white';
+                await delay(2000);
+                refPC.current.style.backgroundColor = 'transparent';
+                refPC.current.style.color = 'black';
+                refMAR.current.style.backgroundColor = 'rgba(0, 128, 0, 0.568)';
+                refMAR.current.style.color = 'white';
+                refMAR.current.value=parseInt(refPC.current.value);
+                await delay(1700);
+                refMAR.current.style.backgroundColor = 'transparent';
+                refMAR.current.style.color = 'black';
+                //////////////////////////////////////////////////
+                refROM.current.value+="T1: PC(I), RAM(E), IR(L)\n";
+                refPC.current.style.backgroundColor = 'rgba(0, 128, 0, 0.568)';
+                refPC.current.style.color = 'white';
+                refPC.current.value=parseInt(refPC.current.value)+1;
+                refRAM.current.style.backgroundColor='rgba(0, 128, 0, 0.568)';
+                refRAM.current.style.color='white';
+                await delay(1500);
+                refRAM.current.style.backgroundColor = 'transparent';
+                refRAM.current.style.color = 'black';
+                refIr.current.style.backgroundColor = 'rgba(0, 128, 0, 0.568)';
+                refIr.current.style.color = 'white';
+                refIr.current.value="MVI";
+                refPC.current.style.backgroundColor = 'transparent';
+                refPC.current.style.color = 'black';
+                refMAR.current.value=parseInt(refPC.current.value);
+                await delay(1700);
+                refIr.current.style.backgroundColor = 'transparent';
+                refIr.current.style.color = 'black';
+                //////////////////////////////////////////////////
+                refROM.current.value+="T2:PC(E), MAR(E,L)\n";
+                refPC.current.style.backgroundColor = 'rgba(0, 128, 0, 0.568)';
+                refPC.current.style.color = 'white';
+                await delay(1500);
+                refPC.current.style.backgroundColor = 'transparent';
+                refPC.current.style.color = 'black';
+                refMAR.current.style.backgroundColor = 'rgba(0, 128, 0, 0.568)';
+                refMAR.current.style.color = 'white';
+                refMAR.current.value=parseInt(refPC.current.value);
+                await delay(1700);
+                refMAR.current.style.backgroundColor = 'transparent';
+                refMAR.current.style.color = 'black';
+                ///////////////////////////////////////////
+                refROM.current.value+="T3: PC(I), RAM(E), A(L)\n";
+                refPC.current.style.backgroundColor = 'rgba(0, 128, 0, 0.568)';
+                refPC.current.style.color = 'white';
+                refPC.current.value=parseInt(refPC.current.value)+1;
+                refRAM.current.style.backgroundColor='rgba(0, 128, 0, 0.568)';
+                refRAM.current.style.color='white';
+                await delay(1500);
+                refRAM.current.style.backgroundColor = 'transparent';
+                refRAM.current.style.color = 'black';
+                refA.current.style.backgroundColor = 'rgba(0, 128, 0, 0.568)';
+                refA.current.style.color = 'white';
                 let num=Number(a.split("\n")[i].split(" ")[1]);    
                 refA.current.value=num;
-                
+                refPC.current.style.backgroundColor = 'transparent';
+                refPC.current.style.color = 'black';
+                refMAR.current.value=parseInt(refPC.current.value);
+                await delay(1700);
+                refA.current.style.backgroundColor = 'transparent';
+                refA.current.style.color = 'black';
+                ////////////////////////////////////////////
+                refROM.current.value+="T4:\n";
+                await delay(1100);
+                ///////////////////////////////////////////
+                refROM.current.value+="T5:\n";
+                await delay(1100);
+                ////////////////////////////////////////////
+                refROM.current.value+="T6: \n";
+                await delay(1100);
+                //////////////////////////////////////////////
                 break;
             case "MOV":
                 if(a.split("\n")[i].split(" ")[1]=="B,A")
@@ -58,22 +138,27 @@ function FlowChart() {
         refA.current.value="";
         refB.current.value="";
         refRAM.current.value="";
+        refROM.current.value="";
+        refPC.current.value="0";
+        refMAR.current.value="0";
+        refIr.current.value="";
     }
 
     return(
     <>
         <div className='flowchart'>
-            <div className='grid'>
-                <TextField id="outlined-read-only-input" multiline rows={3} label="PC" variant="filled"  className='pc' style = {{width: 150}} InputProps={{readOnly: true,}} defaultValue="0"/>
-                <svg width="150" height="50"><line x1="75" y1="0" x2="75" y2="100" stroke="black"/></svg>
-                <TextField id="outlined-basic" multiline rows={3}label="MAR" variant="filled" className='mar' style = {{width: 150}}/>
-                <svg width="150" height="50"><line x1="75" y1="0" x2="75" y2="100" stroke="black"/></svg>
+            <div className='grid' sx={{padding:"0%",margin:0,backgroundColor: "green"}}>
+                <TextField id="outlined-read-only-input" multiline rows={3} label="PC" variant="filled"  inputRef={refPC} className='pc' sx = {{width: 150}} InputProps={{readOnly: true,}} defaultValue="0"/>
+                <svg width="150" height="98"><line x1="75" y1="0" x2="75" y2="98" stroke="black"/></svg>
+                <TextField id="outlined-read-only-input" multiline rows={3}label="MAR" variant="filled" inputRef={refMAR} className='mar' style = {{width: 150}} InputProps={{readOnly: true,}} defaultValue="0"/>
+                <svg width="150" height="98"><line x1="75" y1="0" x2="75" y2="98" stroke="black"/></svg>
                 <TextField id="outlined-basic" multiline rows={3}label="RAM" variant="filled" inputRef={refRAM} className='ram' style = {{width: 150}} onChange={(e) => {setA(e.target.value);}} />
+
             </div>
             <div className='grid'>
-                <svg width="50" height="400">
-                    <line x1="0" y1="350" x2="25" y2="350" stroke="black"/>
-                    <line x1="25" y1="200" x2="25" y2="350" stroke="black"/>
+                <svg width="50" height="500">
+                    <line x1="0" y1="430" x2="25" y2="430" stroke="black"/>
+                    <line x1="25" y1="200" x2="25" y2="430" stroke="black"/>
                     <line x1="25" y1="200" x2="50" y2="200" stroke="black"/>
                 </svg>
             </div>
@@ -84,9 +169,9 @@ function FlowChart() {
             </div>
             <div className='grid'>
                 <TextField id="outlined-read-only-input" multiline rows={3} label="B" variant="filled"  inputRef={refB} style = {{width: 150}} InputProps={{readOnly: true,}} defaultValue="0"/>
-                <svg width="150" height="303">
-                    <line x1="0" y1="97.5" x2="206" y2="97.5" stroke="black"/>
-                    <line x1="75" y1="0" x2="75" y2="97.5" stroke="black"/>
+                <svg width="150" height="403" >
+                    <line x1="0" y1="97.75" x2="206" y2="97.75" stroke="black"/>
+                    <line x1="75" y1="0" x2="75" y2="97.75" stroke="black"/>
                 </svg>
                 
             </div>
@@ -97,9 +182,9 @@ function FlowChart() {
             </div>
             <div className="grid">
                 <TextField id="outlined-read-only-input" multiline rows={3} label="A" variant="filled"  inputRef={refA} style = {{width: 150}} InputProps={{readOnly: true,}} defaultValue="0" />
-                <svg width="150" height="303">
-                    <line x1="0" y1="97.5" x2="206" y2="97.5" stroke="black"/>
-                    <line x1="75" y1="0" x2="75" y2="97.5" stroke="black"/>
+                <svg width="150" height="403">
+                    <line x1="0" y1="97.75" x2="206" y2="97.75" stroke="black"/>
+                    <line x1="75" y1="0" x2="75" y2="98" stroke="black"/>
                 </svg>
             </div>
             <div div className='grid'>
@@ -113,9 +198,9 @@ function FlowChart() {
             <div className='grid'>
                 <div className='alu'>ALU</div>
                 <svg width="200" height="50"></svg>
-                <TextField id="outlined-read-only-input" multiline rows={2}label="IR" variant="filled" className='ir' inputRef={refIr} style = {{width: 150}} InputProps={{readOnly: true,}} defaultValue="0"/>
+                <TextField id="outlined-read-only-input" multiline rows={2} label="IR" variant="filled" className='ir' inputRef={refIr} style = {{width: 150}} InputProps={{readOnly: true,}} defaultValue="0"/>
                 <svg width="200" height="70"><line x1="75" y1="0" x2="75" y2="100" stroke="black"/></svg>
-                <TextField id="outlined-basic" multiline rows={3}label="ROM" variant="filled" className='rom' style = {{width: 150}}/>
+                <TextField id="outlined-read-only-input" multiline rows={7} label="ROM" variant="filled" className='rom' inputRef={refROM} style = {{width: 270}} InputProps={{readOnly: true,}} defaultValue="0"/>
             </div>
         </div>
         <div className="buttons">
