221c3918fb2fbf521b1f08bf2f03993e4fb77f09
Moxesh555 <2020uec1080@mnit.ac.in>
2023-04-18 00:12:45 +0530

    new

--- src/components/FlowChart.js
+++ src/components/FlowChart.js
@@ -24,7 +24,6 @@ function FlowChart() {
 
         const decimalA = parseInt(refA.current.value, 2);
         const decimalB = parseInt(refB.current.value, 2);
-        //const dPC= parseInt(refPC.current.value, 2);
         let command=a.split(" ")[0];
         
         switch (command) 
